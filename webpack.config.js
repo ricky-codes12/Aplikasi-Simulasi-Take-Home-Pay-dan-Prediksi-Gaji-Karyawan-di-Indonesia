0a0851660344581dda2857923a04719b6d82c5eb
fawwazaid <fawwazaid@gmail.com>
2026-01-02 10:06:54 +0700

    update url env

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,37 @@
 const path = require("path");
-const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
 
-plugins: [
-  new BundleAnalyzerPlugin()
-],
-
-
-module.exports = {
-  entry: "./frontend/app.js",
-  output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
-    clean: true,
-  },
-  mode: "production",
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
-      },
-    ],
-  },
-  devServer: {
-    static: {
-      directory: path.join(__dirname, "frontend"),
+plugins: [new BundleAnalyzerPlugin()],
+  (module.exports = {
+    entry: "./frontend/app.js",
+    output: {
+      filename: "bundle.js",
+      path: path.resolve(__dirname, "dist"),
+      clean: true,
     },
-    compress: true,
-    port: 5501,
-    open: true,
-    hot: true,
-  },
-};
+    mode: "production",
+    module: {
+      rules: [
+        {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"],
+        },
+      ],
+    },
+    devServer: {
+      static: {
+        directory: path.join(__dirname, "frontend"),
+      },
+      compress: true,
+      port: 5501,
+      open: true,
+      hot: true,
+    },
+    plugins: [
+      new webpack.DefinePlugin({
+        __API_BASE_URL__: JSON.stringify(
+          process.env.API_BASE_URL || "http://localhost:8000"
+        ),
+      }),
+    ],
+  });
